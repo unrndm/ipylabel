cca9d2884f360882c676b2647b3f8c8ac34a9f65
unrndm <unrndm@gmail.com>
2022-08-17 18:01:34 +0300

    feat: added type validation and new types

--- ipylabel/text.py
+++ ipylabel/text.py
@@ -7,15 +7,16 @@
 """
 Widget for labeling text
 """
-
+from __future__ import annotations
 from ipywidgets import DOMWidget
-from traitlets import Bool, Unicode
+from traitlets import Bool, Unicode, List, Dict, validate
 
 from ._frontend import module_name, module_version
+from .types import Color, Result, ProposalType
 
 
 class TextWidget(DOMWidget):
-    """TODO: Add docstring here"""
+    """Widget providing a way to label data in jupyter"""
 
     _model_name = Unicode("TextLabelingModel").tag(sync=True)
     _model_module = Unicode(module_name).tag(sync=True)
@@ -26,6 +27,35 @@ class TextWidget(DOMWidget):
     _view_module_version = Unicode(module_version).tag(sync=True)
 
     # Your widget state goes here. Make sure to update the corresponding
-    # JavaScript widget state (defaultModelProperties) in widget.ts
-    value = Unicode("Jupyter").tag(sync=True)
-    disabled = Bool(False).tag(sync=True)
+    # JavaScript widget state (defaultModelProperties) in src/TextWidget.tsx
+    
+    # expects as input
+    text = Unicode("", help="text to label").tag(sync=True)
+    labels = List(trait=Unicode(), help="list of labels to label `text` with").tag(s1ync=True)
+    colors = List(trait=Color(), help="list of colors to label `text` with (in hex format)").tag(sync=True)
+
+    # expects as output
+    result = List(trait=Dict(per_key_traits={"start": Unicode(), "end": Unicode(), "label": Unicode()}), default_value=[], help="result of labeling, list of dicts with keys `start`, `end` and `label`").tag(sync=True)
+    finished = Bool(False, help="special state triggered by labeler meaning that there is nothing to label").tag(sync=True)
+
+    # check that each label has color, i.e. they are the same length
+    @validate("labels","colors")
+    def _check_lengths(self, proposal: ProposalType[TextWidget]):
+        # don't want to hardcode 
+        if proposal["trait"]==self.__class__.labels:
+            labels = proposal["value"]
+            colors = proposal["owner"].colors
+
+            if len(labels) != len(colors):
+                raise ValueError("`labels` must be same shape as `colors`")
+
+            return labels
+
+        elif proposal["trait"]==self.__class__.colors:
+            colors = proposal["value"]
+            labels = proposal["owner"].labels
+
+            if len(labels) != len(colors):
+                raise ValueError("`colors` must be same shape as `labels`")
+
+            return colors
---
+++ ipylabel/types.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+from typing import (
+    Any,
+    TypeVar,
+    Generic,
+)
+# can be moved to typing in 3.11
+from typing_extensions import TypedDict
+
+from traitlets import TraitType
+
+class Result(TypedDict):
+    start: str
+    end: str
+    label: str
+
+
+T = TypeVar('T')
+
+class ProposalType(TypedDict, Generic[T]):
+    trait: Any
+    value: Any
+    owner: T
+
+
+POSSIBLE_VALUES: set[str] = {
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+}
+
+
+class Color(TraitType):
+    """A trait for color representations
+
+    should be compatible with CSS3 specification but isn't
+    currently only hexadecimal value on length 6 is supporeted
+    maybe make it a wrapper on https://pydantic-docs.helpmanual.io/usage/types/#color-type
+    """
+
+    default_value = "#000000"
+    info_text = "a color in hexadecimal representation without alpha chanel"
+
+    def validate(self, obj, value):
+        if isinstance(value, str):
+            if len(value) == 7:
+                if all(el in POSSIBLE_VALUES for el in value[1:].lower()):
+                    return value.lower()
+        self.error(obj, value)
+
+    def from_string(self, s: str):
+        if self.allow_none and s == "None":
+            return None
+        if len(s) != 7:
+            raise ValueError("length must be exactly 7, got %d" % len(s))
+        if not s.startswith("#"):
+            raise ValueError("must start with #, got %s" % s[0])
+        if not all(el in POSSIBLE_VALUES for el in s[1:].lower()):
+            raise ValueError("must start with #, got %s" % s[0])
+        return s.lower()
